25cb0bc3eedf81cfb2088313c3bb61a10d10bf61
leyshanth.1177@gmail.com <ley@Leyshanths-MacBook-Air.local>
2025-03-23 13:11:11 +0000

    Simplify webpack configuration to fix build errors

--- next.config.js
+++ next.config.js
@@ -13,19 +13,11 @@ const nextConfig = {
   },
   // Exclude template directory from build
   webpack: (config) => {
-    // Ensure watchOptions exists
-    config.watchOptions = config.watchOptions || {};
-    
-    // Ensure ignored is an array
-    if (!config.watchOptions.ignored) {
-      config.watchOptions.ignored = [];
-    } else if (!Array.isArray(config.watchOptions.ignored)) {
-      // If it's not an array, convert it to one
-      config.watchOptions.ignored = [config.watchOptions.ignored];
-    }
-    
-    // Add template directory to ignored
-    config.watchOptions.ignored.push('**/template/**');
+    // Simply set watchOptions with our pattern
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: /node_modules|.git|template/
+    };
     
     return config;
   },
