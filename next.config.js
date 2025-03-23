923839e2865d23128232714a8691f16283beeca6
leyshanth.1177@gmail.com <ley@Leyshanths-MacBook-Air.local>
2025-03-23 13:08:09 +0000

    Fix build error by excluding template directory from deployment

--- next.config.js
+++ next.config.js
@@ -11,6 +11,15 @@ const nextConfig = {
   images: {
     domains: ['localhost'], // Add your Appwrite domain here
   },
+  // Exclude template directory from build
+  webpack: (config, { isServer }) => {
+    // Ignore the template directory during build
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: [...(config.watchOptions?.ignored || []), '**/template/**']
+    };
+    return config;
+  },
 };
 
 module.exports = withPWA(nextConfig);
